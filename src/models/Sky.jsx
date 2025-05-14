ec68f65140e893ed44430fd460d78aac6d5ccd3c
paulina <paulinalopezholguin1@gmail.com>
2025-05-13 23:57:33 -0600

    Added glb file of island

---
+++ src/models/Sky.jsx
@@ -0,0 +1,13 @@
+import { useGLTF } from '@react-three/drei'
+import React from 'react'
+
+import skyScene from '../assets/3d/sky.glb'
+
+const Sky = () => {
+    const Sky = useGLTF(skyScene);
+  return (
+    <mesh></mesh>
+  )
+}
+
+export default Sky
